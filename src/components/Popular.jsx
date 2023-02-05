f98db09cfd733d5abf5fda05ca69ecf6707924cd
smahamoud <115296163+smahamoud@users.noreply.github.com>
2023-02-05 21:32:36 +0000

    recipe images added

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components"
 
 function Popular() {
 
-    useEffect(() => {
+    const [popular, setPopular] = useState([]);
+  
+  useEffect(() => {
         getPopular();
     },[]);
 
@@ -12,15 +15,42 @@ function Popular() {
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
-        console.log(data);
+        setPopular(data.recipes)
+        console.log(data.recipes)
     };
 
 
 
 
   return (
-    <div>Popular</div>
-  )
+    <div>
+
+      {popular.map((recipe) => {
+        return(
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+        return(
+          <Card>
+            <p>{recipe.title}</p>
+            <img src={recipe.image} alt={recipe.title} />
+          </Card>
+          );
+      })}
+          </Wrapper>
+
+        );
+      })}
+    </div>
+  );
 }
 
+const Wrapper = styled.div`
+margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+min-height: 25rem
+border-radius: 2rem
+`
 export default Popular
