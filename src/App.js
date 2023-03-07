27ba6be7a2759ea250e3bac8ee30d13d1156b826
smahamoud <115296163+smahamoud@users.noreply.github.com>
2023-03-07 20:52:00 +0000

    added intructions and ingredients

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import {BrowserRouter} from "react-router-dom";
-import Cuisine from "./pages/Cuisine";
+import Pages from "./pages/Pages";
+import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
 function App() {
   return (
     <div className="App">
-<BrowserRouter>   
+<BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to="/">Tastyy</Logo>
+        </Nav>  
 <Search />   
 <Category />
 <Pages  />
@@ -14,5 +20,25 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+  padding-left: 1rem;
+`;
+
+const Nav = styled.div`
+  padding: 3rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    color: var(--gray-600);
+    font-size: 2rem;
+  }
+`;
 
 export default App;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -24,8 +24,10 @@ function Searched() {
         {searchedRecipes.map((item) => {
             return(
                 <Card key={item.id}>
+                  <Link to={'/recipe/'+item.id}>
                     <img src={item.image} alt="" />
                     <h4>{item.title}</h4>
+                  </Link>  
                 </Card>
             )
         })}
